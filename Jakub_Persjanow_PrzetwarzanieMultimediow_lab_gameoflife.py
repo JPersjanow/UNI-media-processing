b43b3a0506e8f79f40f3b5d3cd2d76f261dbf745
Persjanow Jakub <jakubpersjanow@gmail.com>
2020-04-22 00:04:59 +0200

    Started working on checking neighbours for border

--- Jakub_Persjanow_PrzetwarzanieMultimediow_lab_gameoflife.py
+++ Jakub_Persjanow_PrzetwarzanieMultimediow_lab_gameoflife.py
@@ -11,36 +11,112 @@ class GameOfLife:
         print(f"type of grid: {type(self.grid)}")
 
     def check_grid(self):
-        for x in range(0, self.grid_size, 1):
-            for y in range(0, self.grid_size, 1):
-                neighbours = self.check_neighbours(x, y, self.grid)
+        with open("grid.txt", "w+") as file:
+            for ix, iy in np.ndindex(self.grid.shape):
+                if self.grid_size - 1> ix > 0 and self.grid_size - 1 > iy > 0:
+                    file.writelines(f"{ix} : {iy}\n")
                 
     
     def check_neighbours(self, position_x: int, position_y: int, grid: np.array) -> list:
         life_check: list = []
-        if self.grid_size > position_x > 0 and self.grid_size > position_y > 0:
-            if grid[position_x - 1][position_y] == 1:
-                life_check.append(True)
-            else:
-                life_check.append(False)
-            if grid[position_x + 1][position_y] == 1:
-                life_check.append(True)
-            else:
-                life_check(False)
-            if grid[position_x][position_y + 1] == 1:
-                life_check.append(True)
-            else:
-                life_check(False)
-            if grid[position_x][position_y - 1] == 1:
-                life_check.append(True)
-            else:
-                life_check(False)
-        elif position_x == 0 and self.grid_size > position_y > 0:
-            
 
-        return life_check 
+        if position_y > 0 and position_y < self.grid_size and position_x > 0 and position_x < self.grid_size:
+            return self.check_neighbours_without_border(position_x, position_y, grid)
+
+        elif position_y > 0 and position_y < self.grid_size and position_x == 0:
+            return self.check_neighbours_when_x_zero(position_x, position_y, grid)
+
+        elif position_y > 0 and position_y < self.grid_size and position_x == self.grid_size:
+            return self.check_neighbours_when_x_max(position_x, position_y, grid)
+        
+        elif 
+        return life_check
+
+def check_neighbours_without_border(self, position_x: int, position_y: int, grid: np.array) -> list:
+    life_check = []
+    if grid[position_x - 1][position_y - 1] == 1:
+        life_check.append(True)
+    else:
+        life_check.append(False)
+    if grid[position_x][position_y - 1] == 1:
+        life_check.append(True)
+    else:
+        life_check.append(False)
+    if grid[position_x + 1][position_y - 1] == 1:
+        life_check.append(True)
+    else:
+        life_check.append(False)
+    if grid[position_x - 1][position_y] == 1:
+        life_check.append(True)
+    else:
+        life_check.append(False)
+    if grid[position_x + 1][position_y] == 1:
+        life_check.append(True)
+    else:
+        life_check.append(False)
+    if grid[position_x - 1][position_y + 1] == 1:
+        life_check.append(True)
+    else:
+        life_check.append(False)
+    if grid[position_x][position_y + 1] == 1:
+        life_check.append(True)
+    else:
+        life_check.append(False)
+    if grid[position_x + 1][position_y + 1] == 1:
+        life_check.append(True)
+    else:
+        life_check.append(False)
+    return life_check
+
+def check_neighbours_when_x_zero(self, position_x: int, position_y:int, grid: np.array) -> list:
+    life_check = []
+    if grid[position_x][position_y - 1] == 1:
+        life_check.append(True)
+    else:
+        life_check.append(False)
+    if grid[position_x][position_y + 1] == 1:
+        life_check.append(True)
+    else:
+        life_check.append(False)
+    if grid[position_x + 1][position_y - 1] == 1:
+        life_check.append(True)
+    else:
+        life_check.append(False)
+    if grid[position_x + 1][position_y] == 1:
+        life_check.append(True)
+    else:
+        life_check.append(False)
+    if grid[position_x + 1][position_y + 1] == 1:
+        life_check.append(True)
+    else:
+        life_check.append(False)
+    return life_check
+
+def check_neighbours_when_x_max(self, position_x: int, position_y:int, grid: np.array) -> list:
+    life_check = []
+    if grid[position_x][position_y - 1] == 1:
+        life_check.append(True)
+    else:
+        life_check.append(False)
+    if grid[position_x][position_y + 1] == 1:
+        life_check.append(True)
+    else:
+        life_check.append(False)
+    if grid[position_x - 1][position_y - 1] == 1:
+        life_check.append(True)
+    else:
+        life_check.append(False)
+    if grid[position_x - 1][position_y] == 1:
+        life_check.append(True)
+    else:
+        life_check.append(False)
+    if grid[position_x - 1][position_y + 1] == 1:
+        life_check.append(True)
+    else:
+        life_check.append(False)
+    return life_check
 
 g = GameOfLife()
 g.print_grid()
 g.check_grid()
-g.print_grid()
+# g.print_grid()
